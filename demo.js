f17c807474d2365fcd5d9ff9b8b24c50b2056391
syeef-test <syeefislam@yahoo.com>
2023-01-01 20:12:29 +0530

    dom manipulation 1st part done and removed app.js

--- demo.js
+++ demo.js
@@ -1,1 +1,23 @@
-console.log('Test file')
+console.log('Test file')// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// // document.title = "123"
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// // document.all[10].textContent = 'Hello';
+// console.log(document.forms);
+
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = "Goodbye";
+// headerTitle.innerHTML = '<h3>Hello</h3>'
+
+
+headerTitle.style.borderBottom = 'solid 3px #000';
+headerTitle.style.fontWeight = "bold";
+headerTitle.style.color = "green";
