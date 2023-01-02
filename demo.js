60ceef735bf69afd198a2467163b9b2784c14114
syeef-test <syeefislam@yahoo.com>
2023-01-02 08:12:20 +0530

    by tag name select

--- demo.js
+++ demo.js
@@ -23,26 +23,64 @@
 // headerTitle.style.color = "green";
 
 
-//GETELEMENTSBYCLASSNAME//
+// //GETELEMENTSBYCLASSNAME//
 
-var  items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello2';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'yellow';
+// var  items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello2';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
+
+
+// //gives error
+// //items.style.backgroundColor = '#f4f4f4';
+
+// for(var i=0;i<items.length;i++){
+//     items[i].style.backgroundColor = '#f4f4f4';
+
+// }
+
+// items[2].style.backgroundColor = 'green';
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight = 'bold';
+//     items[i].style.color = 'red';
+// }
+
+//GETELEMENTS BY TAG//
+
+var  li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello2';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';
 
 
 //gives error
 //items.style.backgroundColor = '#f4f4f4';
 
-for(var i=0;i<items.length;i++){
-    items[i].style.backgroundColor = '#f4f4f4';
+for(var i=0;i<li.length;i++){
+    li[i].style.backgroundColor = '#f4f4f4';
 
 }
 
-items[2].style.backgroundColor = 'green';
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight = 'bold';
-    items[i].style.color = 'red';
+// items[2].style.backgroundColor = 'green';
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight = 'bold';
+//     items[i].style.color = 'red';
+// }
+
+//by class name edit as new li element has no class it will not effect it
+// var  items = document.getElementsByClassName('list-group-item');
+// for(var i=0;i<items.length;i++){
+//     items[i].style.backgroundColor = 'yellow';
+
+// }
+
+
+//by tag edit it will effect all li tag irrespective of class name
+for(var i=0;i<li.length;i++){
+    li[i].style.backgroundColor = 'yellow';
+
 }
